f9e35566e0b76d856129dee58cc6aae69396a8cc
William Lin <4momolmr@gmail.com>
2023-03-18 22:54:20 +0800

    feat: add rotated circle (#1)
    * feat: add rotat circle
    
    * fix: remove unused import

---
+++ src/components/HomePageAnimation.tsx
@@ -0,0 +1,46 @@
+import React, {useEffect, useRef} from 'react'
+import * as THREE from 'three';
+
+export const HomePageAnimation = () => {
+    const canvasRef = useRef();
+
+    useEffect(() => {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+            85,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+
+        const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setClearColor( 0xffffff, 1 );
+
+        const geometry = new THREE.SphereGeometry(5, 30,30);
+        const material = new THREE.PointsMaterial({ color: 0x000, sizeAttenuation: false, size: 1});
+        const sphere = new THREE.Points(geometry, material);
+        scene.add(sphere);
+        camera.position.z = 8;
+
+        function animate() {
+            // sphere.rotation.x += 0.005;
+            sphere.rotation.y += 0.005;
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera)
+        }
+        animate();
+
+        // Cleanup on unmount, otherwise stuff will linger in GPU
+        return () => {
+            renderer.forceContextLoss();
+            renderer.dispose();
+            sphere.geometry.dispose();
+            sphere.material.dispose();
+        };
+    }, []);
+
+    return <canvas ref={canvasRef} />;
+}
