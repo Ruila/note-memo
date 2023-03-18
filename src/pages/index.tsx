a658df5ee3a83d538c496827efaca83a5c865ae1
Ruila <4momolmr@gmail.com>
2023-03-18 22:50:48 +0800

    fix: remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import styles from './index.module.css';
 import { HomePageAnimation } from "@site/src/components/HomePageAnimation";
 
 export default function Home(): JSX.Element {
